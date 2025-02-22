2b654f6a8f0fabefe659d83600a569dae8442a5f
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-02-22 23:13:14 +0100

    test

--- keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_split_3x6_3(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
         KC_CAPS, KC_NO,   KC_NO,    KC_MINS,    KC_EQL,    KC_NO,                               KC_NO,    KC_LBRC,    KC_RBRC,    KC_NO,    KC_F10, KC_F11,
-        KC_LSFT, KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                               KC_F6,    KC_F7,    KC_F8, KC_F9,  KC_12, KC_RSFT,
+        KC_LSFT, KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                               KC_F6,    KC_F7,    KC_F8, KC_F9,  KC_F12, KC_RSFT,
                                             KC_NO, MT(KC_LALT, KC_SPC), KC_LCTL,           KC_TRNS,  KC_LGUI,   MT(KC_RALT, KC_BSPC)
     ),
 

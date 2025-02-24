0b3cc9e4fbe64d82989326f88d44c052564e1c74
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-02-24 13:05:00 +0100

    Mon 24 Feb 13:05:00 CET 2025

--- keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
@@ -1,6 +1,10 @@
 // Copyright 2022 beekeeb
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+// In case you forgor:
+// From QMK's docs:
+// The modifiers (mod) argument to the MT() macro are prefixed with MOD_, not KC_
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -9,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                               KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,       KC_BSLS,
         KC_TAB,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                               KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,    KC_ENT,
         KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                                               KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,    KC_RSFT,
-                                    MO(1),  KC_LALT,    KC_LCTL,                        MO(2),    KC_LGUI,    KC_RALT
+                                    MO(1),  MT(MOD_LALT, KC_SPC),    KC_LCTL,                        MO(2),    KC_LGUI,    MT(MOD_RALT, KC_BSPC)
     ),
 
     [1] = LAYOUT_split_3x6_3(

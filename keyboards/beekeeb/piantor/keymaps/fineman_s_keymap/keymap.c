7b1ce4654a6116cf30c698df764eaa0a1e1dce04
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-02-24 12:42:44 +0100

    Mon 24 Feb 12:42:44 CET 2025

--- keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
@@ -6,10 +6,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_3x6_3(
-        KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                               KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,       KC_BSLS,
-        KC_ESC,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                               KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,    KC_ENT,
+        KC_ESC,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                               KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,       KC_BSLS,
+        KC_TAB,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                               KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,    KC_ENT,
         KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                                               KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,    KC_RSFT,
-                                    MO(2),  MT(KC_LALT, KC_SPC),    KC_LCTL,                        MO(1),    KC_LGUI,    MT(KC_RALT, KC_BSPC)
+                                    MO(1),  MT(KC_LALT, KC_SPC),    KC_LCTL,                        MO(2),    KC_LGUI,    MT(KC_RALT, KC_BSPC)
     ),
 
     [1] = LAYOUT_split_3x6_3(
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,  KC_MPRV,    KC_MNXT,    KC_MPLY,    KC_NO,    KC_PSCR,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
         KC_NO, KC_VOLD,   KC_VOLU,    KC_MUTE,    KC_KB_MUTE,    KC_NO,                               KC_NO,    KC_LBRC,    KC_RBRC,    KC_NO,    KC_F10, KC_F11,
         KC_LSFT, KC_BRID,    KC_BRIU,    MS_BTN1,    KC_F4,    KC_F5,                               KC_F6,    KC_F7,    KC_F8, KC_F9,  KC_F12, KC_RSFT,
-                                            KC_NO, MT(KC_LALT, KC_SPC), KC_LCTL,           KC_TRNS,  KC_LGUI,   MT(KC_RALT, KC_BSPC)
+                                            KC_TRNS, MT(KC_LALT, KC_SPC), KC_LCTL,           KC_LCTL,  KC_LGUI,   MT(KC_RALT, KC_BSPC)
     ),
 
 };

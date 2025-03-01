5fd79b6df0530717e7f05efe32f40839e49a0a88
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-03-01 03:27:53 +0100

    fix: added fucking missing comma in _VIM layer

--- keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_VIM] = LAYOUT_split_3x6_3(
-    KC_NO, KC_NO, C(KC_RGHT),KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    KC_NO, KC_NO, C(KC_RGHT),KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     /*KC_NO, KC_NO, OSL(_DEL), KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,*/
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,

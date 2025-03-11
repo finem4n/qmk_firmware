d401faaa4c1ceddbfd490a49cd5042a503e80370
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-03-11 23:10:13 +0100

    game mode num add arrows; move space from super to lctrl

--- keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/fineman_s_keymap/keymap.c
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,                 KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       KC_Y,   KC_U,   KC_I,       KC_O,    KC_P,      KC_BSLS,
         MT(MOD_LALT, KC_TAB),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                       KC_H,   KC_J,   KC_K,       KC_L,    KC_SCLN,   KC_QUOT,
         KC_LSFT,                KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                       KC_N,   KC_M,   KC_COMM,    KC_DOT,  KC_SLSH,   KC_RSFT,
-                                MT(MOD_LGUI, KC_SPC),   KC_LCTL,    MO(_MEDIA),     MT(MOD_RALT, KC_BSPC),  LT(_VIM, KC_ENT),   MO(_NUM)
+                                   KC_LGUI, MT(MOD_LCTL, KC_SPC),   MO(_MEDIA),     MT(MOD_RALT, KC_BSPC),  LT(_VIM, KC_ENT),   MO(_NUM)
     ),
 
     [_NUM] = LAYOUT_split_3x6_3(
         KC_GRV,     KC_1,   KC_2,   KC_3,       KC_4,       KC_5,                           KC_6,   KC_7,       KC_8,       KC_9,   KC_0,   KC_NO,
         KC_LALT,    KC_NO,  KC_NO,  KC_MINS,    KC_EQL,     KC_NO,                          KC_NO,  KC_LBRC,    KC_RBRC,    KC_NO,  KC_F10, KC_F11,
         KC_LSFT,    KC_F1,  KC_F2,  KC_F3,      KC_F4,      KC_F5,                          KC_F6,  KC_F7,      KC_F8,      KC_F9,  KC_F12, KC_RSFT,
-                                        KC_LGUI,   KC_LCTL,    KC_CAPS,         KC_NO,  KC_NO,   KC_TRNS
+                                        KC_LGUI,   KC_LCTL,    CW_TOGG,         KC_NO,  KC_NO,   KC_TRNS
     ),
 
     [_VIM] = LAYOUT_split_3x6_3(
@@ -82,9 +82,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_GAME_NUM] = LAYOUT_split_3x6_3(
-        KC_GRV,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                               KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,
-        KC_LCTL,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,                               KC_NO,  KC_NO,  KC_NO,  KC_NO,  TG(_GAME),  KC_NO,
-        KC_LSFT,    KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                              KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F12,     KC_RSFT,
+        KC_GRV,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                               KC_NO,  KC_NO,      KC_UP,      KC_NO,      KC_NO,      KC_NO,
+        KC_LCTL,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,                               KC_NO,  KC_LEFT,    KC_DOWN,    KC_RGHT,    TG(_GAME),  KC_NO,
+        KC_LSFT,    KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                              KC_F6,  KC_F7,      KC_F8,      KC_F9,      KC_F12,     KC_RSFT,
                                         KC_NO,  KC_NO,   KC_TRNS,               KC_LGUI,  KC_ENT,    KC_LALT
     ),
 
